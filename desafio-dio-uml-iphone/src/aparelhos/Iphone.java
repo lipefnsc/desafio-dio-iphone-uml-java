10ed9925f3baeea82ae7aba807c9a94e82df58e4
Felipe Fonseca <devfelipefonseca@outlook.com>
2024-06-01 20:31:18 -0300

    programa java criado

---
+++ desafio-dio-uml-iphone/src/aparelhos/Iphone.java
@@ -0,0 +1,49 @@
+package aparelhos;
+
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("LIGANDO PARA O NUMERO: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("ATENDENDO");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("INICIANDO CORREIO DE VOZ");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("EXIBINDO PAGINA: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("ADICIONANDO NOVA ABA");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("ATUALIZANDO PAGINA");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("TOCANDO MUSICA");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("PAUSANDO MUSICA");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("SELECIONANDO MUSICA: " + musica);
+    }
+}
---
+++ desafio-dio-uml-iphone/src/main/App.java
@@ -0,0 +1,13 @@
+package main;
+
+import aparelhos.Iphone;
+
+public class App {
+    public static void main(String[] args) {
+
+        Iphone iphone = new Iphone();
+
+        iphone.exibirPagina("google");
+
+    }
+}
